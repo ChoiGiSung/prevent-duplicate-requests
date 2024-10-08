e75356202988f360efa384d550443ce448c25ca3
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2024-10-09 00:27:41 +0900

    init

---
+++ src/main/kotlin/com/coco/durequest/DurequestApplication.kt
@@ -0,0 +1,11 @@
+package com.coco.durequest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DurequestApplication
+
+fun main(args: Array<String>) {
+	runApplication<DurequestApplication>(*args)
+}
---
+++ src/test/kotlin/com/coco/durequest/DurequestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coco.durequest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DurequestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
